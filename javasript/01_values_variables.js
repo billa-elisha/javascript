06c160c98b7d9b21e96c6c518cd3b7b25474e26e
Billa Elisha <billatengbil@gmail.com>
2024-10-08 10:53:04 +0100

    javascript basics

---
+++ javasript/01_values_variables.js
@@ -0,0 +1,88 @@
+7;
+2.5;
+// numbers values
+
+'billa';
+"billa";
+// strings values
+
+true;
+false;
+// boolean values
+
+null;
+undefined;
+// empty values
+
+// all the above values are call as primitive values
+// ============================================================================
+
+
+// array
+// [2,3,'billa',true,'ama'];
+
+// // objects
+// {
+// name:'biilla';
+// lname:'elisha';
+// }
+// these two are called non primitive data types
+// ======================================================================================
+
+
+// variables = place holder for values
+// var x =5;
+// let y= 56;
+// const z= 'bills';
+
+// how to print to the console
+// let firstName = 'billa';
+// let lastName = 'Ellisha';
+// const mobileNo = 4984040;
+
+// console.log('mky first name is ',firstName);
+// console.log('my last name is ' +lastName);
+// console.log(mobileNo);
+
+// template litral (``): it is use for string interpulatlion
+// console.log(`My first name is ${firstName}`);
+// console.log(`${firstName} ${lastName} ${mobileNo}`);
+
+// operators
+// 1 assignment operator (=)
+// let x = 10;
+// let y = 10;
+
+// arithmati operator (+,-,*,/,%)
+// let z =x+y;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// comparasim operators
+// let b = 10;
+// let c= 10;
+// console.log(b>c);
+// console.log(b<=c);
+// console.log(b>=c);
+// console.log(b<c);
+
+// == or === operators
+// let b = 10;
+// let c= '10';
+// console.log(b==c);//== compares values not data types
+// console.log(b===c); // === compares values as well as datatypes
+
+// const account;
+// account = 'fhafei';
+
+// conditions
+let age = 18;
+if (age>=18){
+    console.log("you are alow to vote");
+}
+else{
+    console.log("you are not alow to vote");
+}
